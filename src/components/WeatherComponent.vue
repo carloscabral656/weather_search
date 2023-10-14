ececa0f35997317f8a396d148b21ef8ceb7ccfe1
Carlos Eduardo <edu.carlos.cec@gmail.com>
2023-10-13 23:13:31 -0300

    Adding icons

--- src/components/WeatherComponent.vue
+++ src/components/WeatherComponent.vue
@@ -2,11 +2,10 @@
     <div id="weather">
         <div id="city">{{ chosenCity.name }}</div>
         <div id="weather_icon">
-            <svg height="300px" width="400px"  viewBox="0 0 57.587 57.587" xmlns="http://www.w3.org/2000/svg"><title/><circle cx="28.398" cy="28.696" fill="#f5ce42" r="16.948"/>
-            </svg>
+            <img :src=icon id="weather_img">
         </div>
         <div id="temperature"> {{ currentWeather.temperature }} <span id="scale">K</span> </div>
-        <div id="weather_description">{{ currentWeather.description.toUpperCase() }}</div>
+        <div id="weather_description">{{ currentWeather.main.toUpperCase() }}</div>
         <div id="other_informations">
             <div class="information">
                 <div class="title">Wind</div>
@@ -48,7 +47,8 @@
 }
 
 #weather_img{
-    justify-content: center;
+    width: 100px;
+    height: 100px;
 }
 
 #temperature{
@@ -92,7 +92,8 @@
 <script lang="ts">
 import City from '@/entites/City';
 import Weather from '@/entites/Weather';
-import { capitalize, defineComponent } from 'vue';
+import { icon } from '@fortawesome/fontawesome-svg-core';
+import { defineComponent } from 'vue';
 import { useStore } from 'vuex';
 
 export default defineComponent({
@@ -104,6 +105,11 @@ export default defineComponent({
 
         currentWeather(): Weather{
             return this.store.getters.currentWeather
+        },
+
+        icon() : string{ 
+            let t = this.store.getters.currentWeather.icon
+            return `https://openweathermap.org/img/wn/${t}@2x.png`
         }
     },
     setup(){
